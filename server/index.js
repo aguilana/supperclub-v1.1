7913d109fcb6abb45b6f32db5766dc4b72d1fc62
aguilana <naguila.mandich@gmail.com>
2023-04-12 14:52:02 -0400

    seed

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const { db } = require('./db')
 const PORT = process.env.PORT || 8080
 const app = require('./app')
 // changed to bin // back to script
-const seed = require('../script/seed');
+const seed = require('../script/seed.js');
 
 
 const init = async () => {
