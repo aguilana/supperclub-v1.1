5835791a66645661547339cdc97124ecbff8451c
aguilana <naguila.mandich@gmail.com>
2023-04-12 13:46:52 -0400

    i

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const { db } = require('./db')
 const PORT = process.env.PORT || 8080
 const app = require('./app')
+// changed to bin
 const seed = require('../bin/seed');
 
 const init = async () => {
