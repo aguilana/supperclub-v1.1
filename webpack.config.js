d07d576c791612e68f74f4812fdf3a0f6f976a5a
aguilana <naguila.mandich@gmail.com>
2023-04-18 11:06:38 -0400

    webpack source altered

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,37 @@
 const Dotenv = require('dotenv-webpack');
-module.exports = {
-  mode: 'production',
-  entry: ['mapbox-gl/dist/mapbox-gl.css', './client/index.js'],
-  output: {
-    path: __dirname + '/public',
-    filename: 'bundle.js',
-  },
-  context: __dirname,
-  devtool: 'source-map',
-  // this allows .env to happen
-  plugins: [
-    new Dotenv({
-      path: '.env', // or '.env.local', '.env.[mode]', etc.
-      systemvars: true, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        // this allows css styling loader
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-        options: {
-          presets: ['@babel/preset-env', '@babel/preset-react'],
-        },
-      },
+module.exports = (env) => {
+  return {
+    mode: env ? 'production' : 'development',
+    entry: ['mapbox-gl/dist/mapbox-gl.css', './client/index.js'],
+    output: {
+      path: __dirname + '/public',
+      filename: 'bundle.js',
+    },
+    context: __dirname,
+    devtool: env ? 'hidden-nosources-source-map' : 'inline-source-map',
+    // this allows .env to happen
+    plugins: [
+      new Dotenv({
+        path: '.env', // or '.env.local', '.env.[mode]', etc.
+        systemvars: true, // load all the predefined 'process.env' variables which will trump anything local per dotenv specs.
+      }),
     ],
-  },
+    module: {
+      rules: [
+        {
+          // this allows css styling loader
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      ],
+    },
+  }
 };
